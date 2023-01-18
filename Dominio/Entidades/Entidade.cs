ea32a5bf09e678b7a9aa440329d3454ac73751fb
devspacerj <rf2014souza@gmail.com>
2023-01-17 22:04:50 -0300

    feature/17-01 criação da entidade de perguntas

---
+++ Dominio/Entidades/Entidade.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio.Entidades
+{
+	public class Entidade
+	{
+		public Guid Id { get; set; }
+		public DateTime DataDeCriacao { get; set; }
+	}
+}
---
+++ Dominio/Entidades/Pergunta.cs
@@ -0,0 +1,49 @@
+﻿using Dominio.Services.Notificacao;
+using Flunt.Notifications;
+using Flunt.Validations;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio.Entidades
+{
+	public class Pergunta : Entidade
+	{
+		public Pergunta()
+		{
+		}
+
+		public Pergunta(string titulo, string autor, string assunto)
+		{
+			ValidarTitulo(titulo);
+			ValidarAssunto(assunto);
+			ValidarAutor(autor);
+		}
+
+
+		public string Titulo { get; private set; }
+		public string Autor { get; private set; }
+		public string Assunto { get; private set; }
+
+		private void ValidarTitulo(string titulo)
+		{
+			
+			Titulo = titulo;
+		}
+
+		private void ValidarAutor(string autor)
+		{
+
+			Autor = autor;
+		}
+
+		private void ValidarAssunto(string assunto)
+		{
+
+			Assunto = assunto;
+		}
+	}
+}
