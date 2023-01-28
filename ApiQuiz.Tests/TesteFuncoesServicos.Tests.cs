4de70c8ba77482a75ef53eb1398b58fd32a44dbe
devspacerj <rf2014souza@gmail.com>
2023-01-28 08:09:51 -0300

    featue/28-01 criando testes da entidade de usuario

---
+++ ApiQuiz.Tests/TesteFuncoesServicos.Tests.cs
@@ -0,0 +1,19 @@
+﻿using Dominio.Entidades;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ApiQuiz.Tests
+{
+	[TestClass]
+	public class TesteFuncoesServicos
+	{
+		
+		[TestMethod]
+		[DataRow("","","")]
+		public void Return_false_Ao_Cadastrar_usuario (string email, string senha, string nome) 
+		{
+			var result = new Usuario(nome,email,senha).IsValid;
+
+			Assert.IsFalse(result,"1 should not be prime");
+		}
+	}
+}
---
+++ ApiQuizTests/UnitTest1.cs
@@ -0,0 +1,29 @@
+using Dominio.Entidades;
+
+namespace ApiQuizTests
+{
+	[TestClass]
+	public class UnitTest1
+	{
+		[TestMethod]
+		[DataRow("","","")]
+		[DataRow(" "," "," ")]
+		[DataRow("","teste@teste.com","teste.teste")]
+		[DataRow("","","teste.teste")]
+		[DataRow("","teste@teste.com","")]
+		[DataRow("","teste","")]
+		public void TestUsuarioFalse(string nome, string email, string senha)
+		{
+			var resultado = new Usuario(nome, email, senha).IsValid;
+			Assert.IsFalse(resultado,"Passou");
+		}
+
+		[TestMethod]
+		[DataRow("teste","teste@teste.com","teste.teste")]
+		public void TestUsuarioTrue(string nome,string email,string senha)
+		{
+			var resultado = new Usuario(nome,email,senha).IsValid;
+			Assert.IsTrue(resultado,"Passou");
+		}
+	}
+}
