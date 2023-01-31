2cff8c1f1f57ee6d9ced1cfbae93a91d778eb38c
devspacerj <rf2014souza@gmail.com>
2023-01-30 23:25:53 -0300

    feature/30-01 ajuste na IoC dbcontext

--- Configuracao/Configs/Metadado.cs
+++ Configuracao/Configs/Metadado.cs
@@ -1,10 +1,10 @@
-﻿//namespace Configuracao.Configs
-//{
-//    internal class Metadado
-//    {
-//        internal static object GetAssembly()
-//        {
-//            return typeof(Metadado).Assembly;
-//        }
-//    }
-//}
+﻿namespace Configuracao.Configs
+{
+    internal class Metadado
+    {
+        internal static object GetAssembly()
+        {
+            return typeof(Metadado).Assembly;
+        }
+    }
+}
