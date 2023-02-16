98bcbee0037061d93aa99141d5160fd8b6b8b8c5
devspacerj <rf2014souza@gmail.com>
2023-02-15 22:19:28 -0300

    feature/15-02 aplicando IoC e implementando autenticação

---
+++ Autenticacao/AutenticacaoUsuario.cs
@@ -0,0 +1,37 @@
+﻿
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace Autenticacao
+{
+	public static class AutenticacaoUsuario
+	{
+		private static readonly IConfiguration _config;
+
+
+		public static string Autenticate()
+		{
+			var claims = new[]
+			  {
+					new Claim(JwtRegisteredClaimNames.Sub, "username"),
+					new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+				};
+
+			var key = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(_config["JwtSettings:SecretKey"]));
+			var creds = new SigningCredentials(key,SecurityAlgorithms.HmacSha256);
+
+			var token = new JwtSecurityToken(
+				issuer: null,
+				audience: null,
+				claims: claims,
+				expires: DateTime.UtcNow.AddMinutes(30),
+				signingCredentials: creds
+				);
+
+			return token.ToString();
+		}
+	}
+}
--- Configuracao/Configs/ConfigDependencia.cs
+++ Configuracao/Configs/ConfigDependencia.cs
@@ -1,6 +1,9 @@
 ﻿
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using System.Text;
 
 namespace Configuracao.Configs
 {
@@ -9,6 +12,8 @@ namespace Configuracao.Configs
 		public static IServiceCollection AddConfig(
 			this IServiceCollection services,IConfiguration config)
 		{
+			services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+			services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme);
 			var assemblies = new[]
 			{
 				typeof(IConfiguration).Assembly,
