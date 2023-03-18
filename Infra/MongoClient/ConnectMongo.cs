bf86d024b24c0020138057835f5ac7db15b1c2a4
devspacerj <rf2014souza@gmail.com>
2023-03-18 18:54:49 -0300

    feature/18-03 criando as classes para mongoDb

---
+++ Infra/MongoClient/ConnectMongo.cs
@@ -0,0 +1,13 @@
+﻿
+namespace Infra.MongoClient;
+
+	public class ConnectMongo
+	{
+		public string ConnectionString { get; set; } = null!;
+
+		public string DatabaseName { get; set; } = null!;
+
+	    public string CollectionName { get; set; } = null!;
+
+	}
+
