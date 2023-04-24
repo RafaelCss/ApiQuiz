2383ab6cce8859b87fb5c45b170d4a1ed9e4a2cc
rafaelcss <rf2014souza@gmail.com>
2023-04-24 18:43:52 -0300

    24-04 ajuste arquivos

--- Api-quiz/Program.cs
+++ Api-quiz/Program.cs
@@ -3,6 +3,7 @@ using Configuracao.ConfigDominio;
 
 using Configuracao.Configs;
 using Configuracao.MongoConfig;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 // Add services to the container
@@ -17,7 +18,32 @@ builder.Services.AddConfig(config);
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options =>
+{
+	// ... código omitido
 
+	options.AddSecurityDefinition("Bearer",new OpenApiSecurityScheme
+	{
+		Description = "JWT Authorization header using the Bearer scheme.",
+		Type = SecuritySchemeType.Http,
+		Scheme = "bearer"
+	});
+
+	options.AddSecurityRequirement(new OpenApiSecurityRequirement
+	{
+		{
+			new OpenApiSecurityScheme
+			{
+				Reference = new OpenApiReference
+				{
+					Type = ReferenceType.SecurityScheme,
+					Id = "Bearer"
+				}
+			},
+			Array.Empty<string>()
+		}
+	});
+});
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
--- Dominio/Services/Comandos/ComandoPerquntas.cs
+++ Dominio/Services/Comandos/ComandoPerquntas.cs
@@ -7,90 +7,90 @@ using MongoDB.Driver;
 
 namespace Dominio.Services.Comandos;
 
-	public class ComandoPerquntas : Comando, IComandoPerguntas
+public class ComandoPerquntas : Comando, IComandoPerguntas
+{
+	private readonly IMongoRepositorio<PerguntasMongo> _mongoRepositorio;
+	private readonly string collection = typeof(PerguntasMongo).Name;
+
+	public ComandoPerquntas(IMongoRepositorio<PerguntasMongo> mongoRepositorio)
 	{
-		private readonly IMongoRepositorio<PerguntasMongo> _mongoRepositorio;
-		private readonly string collection = typeof(PerguntasMongo).Name;
+		_mongoRepositorio = mongoRepositorio;
+	}
 
-		public ComandoPerquntas(IMongoRepositorio<PerguntasMongo> mongoRepositorio)
-		{
-			_mongoRepositorio= mongoRepositorio;
-		}
-
-		public async Task<ApiResponse> BuscarPergunta(string campo,string valor)
-		{
+	public async Task<ApiResponse> BuscarPergunta(string campo,string valor)
+	{
 		//var filter =Builders<PerguntasMongo>.Filter.And(
 		//				Builders<PerguntasMongo>.Filter.Eq(campo,valor),
 		//				Builders<PerguntasMongo>.Filter.Eq(campo,valor));
 
 		var filter = Builders<PerguntasMongo>.Filter.Eq(campo,valor);
-			var pergunta = _mongoRepositorio.GetAsyncFiltro(this.collection,filter);
+		var pergunta = await _mongoRepositorio.GetAsyncFiltro(this.collection,filter);
 
-			var response = new ApiResponse
-			(
-				 true,
-				"Retorno",
-				new { pergunta }
+		var response = new ApiResponse
+		(
+			 true,
+			"Retorno",
+			new { pergunta }
 
-			);
+		);
 
-			return response;
-		}
-
-		public async Task<ApiResponse> BuscarPerguntas(string campo,string valor)
-		{
-			var perguntas = _mongoRepositorio.GetAsync(this.collection);
-
-			var response = new ApiResponse
-			(
-				 true,
-				"Retorno",
-				new { perguntas }
-			);
-
-			return response;
-		}
-
-		public async Task<ApiResponse> CadastrarPergunta(string titulo, string assunto, string autor)
-		{
-			var validarPergunta = new Pergunta(titulo,autor,assunto);
-
-			if(!validarPergunta.IsValid)
-			{
-				return new ApiResponse
-				(
-					false,
-					"Retorno",
-					new { validarPergunta.Notifications}
-				);
-			}
-			var pergunta = new PerguntasMongo { Assunto = assunto,Autor_id = autor,Titulo = titulo };
-			var repositorio = _mongoRepositorio.CreateAsync(pergunta,this.collection);
-			var response = new ApiResponse
-				(
-					 true,
-					"Retorno",
-					new { repositorio }
-
-				);
-			return response;
-		}
-
-		public async Task<ApiResponse> DeletarPergunta(string id)
-		{
-			var repositorio = _mongoRepositorio.DeleteAsync(id,this.collection);
-
-			var response = new ApiResponse(true,"feito",new
-			{
-				repositorio,
-			});
-
-			return response;
-		}
-
-		public Task<ApiResponse> EditarPergunta(string id,string nome,string email,string senha)
-		{
-			throw new NotImplementedException();
-		}
+		return response;
 	}
+
+	public async Task<ApiResponse> BuscarPerguntas(string campo,string valor)
+	{
+		var perguntas = await _mongoRepositorio.GetAsync(this.collection);
+
+		var response = new ApiResponse
+		(
+			 true,
+			"Retorno",
+			new { perguntas }
+		);
+
+		return response;
+	}
+
+	public async Task<ApiResponse> CadastrarPergunta(string titulo,string assunto,string autor)
+	{
+		var validarPergunta = new Pergunta(titulo,autor,assunto);
+
+		if(!validarPergunta.IsValid)
+		{
+			return new ApiResponse
+			(
+				false,
+				"Retorno",
+				new { validarPergunta.Notifications }
+			);
+		}
+		var pergunta = new PerguntasMongo { Assunto = assunto,Autor_id = autor,Titulo = titulo };
+		var repositorio = _mongoRepositorio.CreateAsync(pergunta,this.collection);
+		var response = new ApiResponse
+			(
+				 true,
+				"Retorno",
+				new { repositorio }
+
+			);
+		return response;
+	}
+
+	public async Task<ApiResponse> DeletarPergunta(string id)
+	{
+		var repositorio = await _mongoRepositorio.DeleteAsync(id,this.collection);
+
+		var response = new ApiResponse(true,"feito",new
+		{
+			repositorio,
+		});
+
+		return response;
+	}
+
+	public Task<ApiResponse> EditarPergunta(string id,string nome,string email,string senha)
+	{
+		throw new NotImplementedException();
+	}
+}
 
